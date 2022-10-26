6a12173e741fe134eb29a65a50f93d2cb78bc0bd
Brian Poteet <brian.n.poteet@gmail.com>
2022-10-25 21:48:54 -0500

    Only display completed units in charts

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@
 import { ChartConfiguration } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { Project } from '../project';
+import { ProjectStatus } from '../project-status';
 
 @Component({
   selector: 'app-charts',
@@ -11,14 +12,13 @@ import { Project } from '../project';
 export class ChartsComponent implements OnInit, OnChanges {
   @Input() projects: Project[] = [];
 
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
-  title = 'ng2-charts-demo';
+  @ViewChild(BaseChartDirective) allProjectsChart: BaseChartDirective | undefined;
+  @ViewChild(BaseChartDirective) completedProjectsChart: BaseChartDirective | undefined;
 
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
+  public completedProjectsData: ChartConfiguration<'bar'>['data'] = {
     labels: [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ],
     datasets: [
       { data: [], label: 'Council Districts' },
@@ -40,15 +40,16 @@ export class ChartsComponent implements OnInit, OnChanges {
   }
 
   updateCharts() {
-    this.barChartData.datasets[0].data = [];
+    this.completedProjectsData.datasets[0].data = [];
     for (let i = 1; i < 11; i++) {
-      var districtProjects = this.projects.filter(project => Math.floor(project.council_district) == i);
+      var districtProjects = this.projects.filter(project => (Math.floor(project.council_district) == i) && project.development_status === ProjectStatus.ProjectCompleted);
       var unitCount = 0;
       for (const project of districtProjects) {
         unitCount = unitCount + Number(project.total_units);
       }
-      this.barChartData.datasets[0].data.push(unitCount);
+      this.completedProjectsData.datasets[0].data.push(unitCount);
     } 
-    this.chart?.update();
+
+    this.completedProjectsChart?.update();
   }
 }
